4ce9f4207ca2c4c47ce742326fc117a41e2f2d74
indian0ch <fesiukandrey@gmail.com>
2023-07-18 17:14:51 +0300

    Added catalog page and start layout

---
+++ src/components/Catalog/CatalogPage.js
@@ -0,0 +1,13 @@
+import classes from "./Catalog.module.css";
+
+function CatalogPage(props) {
+  return (
+    <div className={`container-md my-5`}>
+      <div className="col-md-8 col-12 mx-auto">
+        <h2>Каталог фільмів</h2>
+        <div className="my-4"></div>
+      </div>
+    </div>
+  );
+}
+export default CatalogPage;
