ef49e869c2e6ba524c3ed4269b56ebf6ba18fdaa
indian0ch <fesiukandrey@gmail.com>
2023-07-25 19:07:33 +0300

    Remove console.logs

--- src/components/Authorization/fetchUser.js
+++ src/components/Authorization/fetchUser.js
@@ -8,11 +8,9 @@ export const fetchUser = (url, dataObject) => {
     body: JSON.stringify(dataObject),
   })
     .then((response) => {
-      console.log(response);
       return response.json();
     })
     .then((data) => {
-      console.log(data);
       return data;
     })
     .catch(() => {
