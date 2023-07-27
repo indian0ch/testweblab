0ae2a8886cfbb6a6a3e37eecf298e462902b9ba4
indian0ch <fesiukandrey@gmail.com>
2023-07-27 14:55:11 +0300

    Fixed 6. Changed link because adding to arr its backend specific. New link will get film by desc in id, so new film will be at first page

--- src/asserts/urlLinks.js
+++ src/asserts/urlLinks.js
@@ -3,5 +3,5 @@ export const SESSION_URL = `${process.env.REACT_APP_API_URL}/sessions`;
 export const DELETE_URL = `${process.env.REACT_APP_API_URL}/movies/`;
 export const ADD_URL = `${process.env.REACT_APP_API_URL}/movies`;
 export const IMPORT_URL = `${process.env.REACT_APP_API_URL}/movies/import`;
-export const LIST_URL = `${process.env.REACT_APP_API_URL}/movies?limit=5`;
+export const LIST_URL = `${process.env.REACT_APP_API_URL}/movies?limit=5&sort=id&order=DESC`;
 export const SortLIST_URL = `${process.env.REACT_APP_API_URL}/movies?sort=title&order=ASC&limit=5`;
